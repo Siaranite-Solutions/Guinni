75b66e308dda905bc9c233d963b64f4e124adb61
Arawn-Davies <arawn.davies@gmail.com>
2019-01-02 02:07:28 +0000

    Added bin file to gitignore and setup IDT/GDT in kernel

--- Source/src/kernel.c
+++ Source/src/kernel.c
@@ -1,8 +1,13 @@
 //#include <stdio.h>
 
 #include <kernel/tty.h>
+#include <kernel/idt.h>
+#include <kernel/gdt.h>
+
 
 void kernel_main(void) {
+	init_gdt();
+	init_idt();
 	terminal_initialize();
 	terminal_writestring("Hello, kernel World!");
 }
